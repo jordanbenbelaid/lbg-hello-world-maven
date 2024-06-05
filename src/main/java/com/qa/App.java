c242c2e092dea5f5e5a4c17e983c693a594d21e0
Jordan B <jordanbenbelaid@hotmail.com>
2024-06-05 10:15:26 +0100

    Added a jenkinsfile to help with our pipeline

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -12,8 +12,8 @@ public class App
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
-        System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Jordan"));
+        System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }
 
